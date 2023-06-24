2f58ca0728062f914eec16af93b0db7f94e09416
Mattheus <mattheusp382@gmail.com>
2023-06-24 12:47:11 -0300

    commit_v250

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // var el = document.querySelector('fa-solid fa-bars menu-mobile-icon');
 
 // el.addEventListener('click',()=>{
-    //Queremos mostrar o menu.
-   // var menuItems = document.querySelector('.menu-items');
-   // menuItems.classList.add('show');
+    // Queremos mostrar o menu.
+    // var menuItems = document.querySelector('.menu-items');
+    // menuItems.classList.add('show');
 // });
